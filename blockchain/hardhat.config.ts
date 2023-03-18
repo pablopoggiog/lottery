1ac4d6f717a18bfee1b10ce3a207a07fcbad08a9
Pablo Poggio <pablopoggiog@gmail.com>
2023-03-18 13:22:04 -0300

    Set up env vars

--- blockchain/hardhat.config.ts
+++ blockchain/hardhat.config.ts
@@ -1,8 +1,11 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+
+dotenv.config({ path: __dirname + "/../.env" });
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.18",
+  solidity: "0.8.18"
 };
 
 export default config;
