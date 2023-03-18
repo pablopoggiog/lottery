4bf4cb1a1aada4d9237c433eaa038d6d09b061ad
Pablo Poggio <pablopoggiog@gmail.com>
2023-03-18 13:22:45 -0300

    Configure sepolia within hardhat

--- blockchain/hardhat.config.ts
+++ blockchain/hardhat.config.ts
@@ -4,8 +4,17 @@ import * as dotenv from "dotenv";
 
 dotenv.config({ path: __dirname + "/../.env" });
 
+const INFURA_API_KEY = process.env.INFURA_API_KEY;
+const SEPOLIA_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY!;
+
 const config: HardhatUserConfig = {
-  solidity: "0.8.18"
+  solidity: "0.8.18",
+  networks: {
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [SEPOLIA_PRIVATE_KEY]
+    }
+  }
 };
 
 export default config;
