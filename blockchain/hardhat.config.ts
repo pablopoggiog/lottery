bcdc99bcb0042879575a5a6ba28bdffc89ba6bb5
Pablo Poggio <pablopoggiog@gmail.com>
2023-03-18 13:31:18 -0300

    Simplify env vars config

--- blockchain/hardhat.config.ts
+++ blockchain/hardhat.config.ts
@@ -2,7 +2,7 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import * as dotenv from "dotenv";
 
-dotenv.config({ path: __dirname + "/../.env" });
+dotenv.config();
 
 const INFURA_API_KEY = process.env.INFURA_API_KEY;
 const SEPOLIA_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY!;
