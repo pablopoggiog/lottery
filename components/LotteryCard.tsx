921533b4aaf562d19586f59289aaf91e8c6079ce
Pablo Poggio <pablopoggiog@gmail.com>
2023-03-18 21:54:28 -0300

    Add transition to pot board

--- components/LotteryCard.tsx
+++ components/LotteryCard.tsx
@@ -1,3 +1,4 @@
+import { useAutoAnimate } from "@formkit/auto-animate/react";
 import truncateEthAddress from "truncate-eth-address";
 import style from "../styles/PotCard.module.css";
 import { useAppContext } from "../context/context";
@@ -5,13 +6,15 @@ import { useAppContext } from "../context/context";
 const LotteryCard = () => {
   const { address, enterLottery, lotteryPot } = useAppContext();
 
+  const [parent] = useAutoAnimate();
+
   return (
     <div className={style.wrapper}>
       <div className={style.title}>
         {/* TODO: Dynamically render the lotteryID */}
         Lottery <span className={style.textAccent}>#1</span>
       </div>
-      <div className={style.pot}>
+      <div className={style.pot} ref={parent}>
         Pot 🍯: <span className={style.goldAccent}>{lotteryPot} ETH</span>
       </div>
 
